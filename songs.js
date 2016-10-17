8803e97cebe6f33bc591093687fe04923cdc96df
LaDonna <boxgirl85@gmail.com>
2016-10-17 10:48:39 -0500

    completed exercise 2

---
+++ songs.js
@@ -0,0 +1,44 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+//Adding songs to beginning and end of the array
+songs.push("Koennt ihr mich horen by Xavier Naidoo on the album Nicht von dieser Welt");
+
+songs.unshift("Vom selben Stern by Ich & Ich on the album Sterne");
+// console.log(songs);
+
+for (var i = 0; i < songs.length; i++) {
+	songs[i] = songs[i].replace('>', '');
+
+	 if(songs[i].indexOf('*') !== -1)
+    {
+        songs[i] = songs[i].replace('*', '');
+    }
+     if(songs[i].indexOf('@') !== -1)
+    {
+        songs[i] = songs[i].replace('@', '');
+    }
+     if(songs[i].indexOf('(') !== -1)
+    {
+        songs[i] = songs[i].replace('(', '');
+    }
+     if(songs[i].indexOf('!') !== -1)
+    {
+        songs[i] = songs[i].replace('!', '');
+    }
+}
+console.log(songs);
+
+//writing string to the div on the HTML
+
+var insert = document.getElementById("maincontent");
+
+insert.innerHTML = "<div>" + songs[0] + "</div>" + "<div>" + songs[1] + "</div>" + "<div>" + songs[2] + "</div>"
++ "<div>" + songs[3] + "</div>" + "<div>" + songs[4] + "</div>" + "<div>" + songs[5] + "</div>" + "<div>" + songs[6] + "</div>";
+
+
